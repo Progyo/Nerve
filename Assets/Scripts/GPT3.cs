a6ce2aa7cb092f08eecb23b73e48e9bc65e604ae
Progyo <nadim@darava.com>
2021-08-08 17:08:35 +0200

    Added secret.json. Working on GPT3 OpenAI interface.

---
+++ Assets/Scripts/GPT3.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+/// <summary>
+/// Interface which connects to Open-AI's GPT-3
+/// </summary>
+public static class GPT3
+{
+
+    /// <summary>
+    /// Open-AI API Key
+    /// </summary>
+    private static string token = "";
+    
+
+
+
+    /// <summary>
+    /// Load api key
+    /// </summary>
+    private static void Init() 
+    {
+        //Only fetch token if it hasn't already been fetched
+        if(token == "")
+        {
+            StreamReader stream = File.OpenText("secret.json");
+            string text = stream.ReadToEnd();
+
+            //https://stackoverflow.com/questions/16652763/parsing-json-to-not-objects-in-net-4-0
+            JToken data = JObject.Parse(text);
+            token = (string) data["key"];
+        }
+    }
+
+}
